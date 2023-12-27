11a6cd8546f2eda92cee22f9acad39956aeb422c
“acrotrucker” <luchkovalerii@gmail.com>
2023-12-26 21:51:34 -0500

    rename to homework2

--- homework1/print_numbers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Loop to print numbers from 1 to 10
-for ((i=1; i<=10; i++)); do
-    echo $i
-done
-
--- homework1/user_input.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ask the user for their name
-echo -n "Enter your name: "
-read name
-
-# Greet the user
-echo "Hello, $name! Nice to meet you."
-
